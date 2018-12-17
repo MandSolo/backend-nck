7d31a07d25b33e89119dd55d3c4d4770fadf9496
MandSolo <mandcashin@hotmail.co.uk>
2018-12-17 12:24:34 +0000

    implemented all up and down functions

--- db/migrations/20181217113226_create_articles_table.js
+++ db/migrations/20181217113226_create_articles_table.js
@@ -1,8 +1,16 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('articles', (articlesTable) => {
+    articlesTable.increments('article_id').primary();
+    articlesTable.string('title');
+    articlesTable.string('body');
+    articlesTable.integer('votes').defaultTo(0);
+    articlesTable.string('topic').references('topics.slug');
+    articlesTable.string('username').references('users.username');
+    articlesTable.timestamp('created_at').defaultTo(knex.fn.now());
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('articles');
 };
--- db/migrations/20181217113233_create_comments_table.js
+++ db/migrations/20181217113233_create_comments_table.js
@@ -1,8 +1,15 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('comments', (commentsTable) => {
+    commentsTable.increments('comment_id').primary();
+    commentsTable.string('username').references('users.username');
+    commentsTable.integer('article_id').references('articles.article_id');
+    commentsTable.integer('votes').defaultsTo(0);
+    commentsTable.timestamp('created_at').defaultTo(knex.fn.now());
+    commentsTable.string('body');
+  });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('comments');
 };
