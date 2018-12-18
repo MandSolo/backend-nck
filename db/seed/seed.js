fe6ecab1ec813849af751a13c19920ffb8cd4a89
MandSolo <mandcashin@hotmail.co.uk>
2018-12-18 11:55:51 +0000

    step 1 - seeding complete

--- db/seed/seed.js
+++ db/seed/seed.js
@@ -1,16 +1,29 @@
 
 const {
-  topicData, userData,
-  // articleData, commentData,
+  topicData, userData, articleData, commentData,
 } = require('../data');
-// const { } = require('../utils');
+const {
+  userLookup, formatArticleData, articleLookup, formatCommentData,
+} = require('../utils');
 
 exports.seed = function (knex, Promise) {
   return Promise.all([
     knex('topics').del(),
     knex('users').del(),
+    knex('articles').del(),
+    knex('comments').del(),
   ])
 
     .then(() => knex('topics').insert(topicData).returning('*'))
-    .then(() => knex('users').insert(userData).returning('*'));
+    .then(() => knex('users').insert(userData).returning('*'))
+    .then((usersRows) => {
+      const userLookupObj = userLookup(usersRows);
+      const formattedCommentData = formatArticleData(articleData, userLookupObj);
+      return Promise.all([userLookupObj, knex('articles').insert(formattedCommentData).returning('*')]);
+    })
+    .then(([userLookObj, articleRows]) => {
+      const articleLookupObj = articleLookup(articleRows);
+      const formattedCommentData = formatCommentData(commentData, articleLookupObj, userLookObj);
+      return knex('comments').insert(formattedCommentData).returning('*');
+    });
 };
--- db/utils/index.js
+++ db/utils/index.js
@@ -1,3 +1,30 @@
+exports.userLookup = users => users.reduce((accUserObj, currentUser) => {
+  accUserObj[currentUser.username] = currentUser.username;
+  return accUserObj;
+}, {});
 
-// exports.formatTopicData = topicData => topicData.map(topicDatum => (
-//   { topic_slug: topicDatum.slug, topic_description: topicDatum.description }));
+exports.formatArticleData = (data, userLookup) => {
+  const formattedArticleData = data.map(({ created_by, created_at, ...otherkeys }) => ({
+    ...otherkeys,
+    username: userLookup[created_by],
+    created_at: new Date(created_at),
+  }));
+  return formattedArticleData;
+};
+
+exports.articleLookup = articles => articles.reduce((accArticleObj, currentArticle) => {
+  accArticleObj[currentArticle.title] = currentArticle.article_id;
+  return accArticleObj;
+}, {});
+
+exports.formatCommentData = (data, articleLookup, userLookup) => {
+  const formattedCommentData = data.map(({
+    created_at, created_by, belongs_to, ...otherkeys
+  }) => ({
+    ...otherkeys,
+    username: userLookup[created_by],
+    created_at: new Date(created_at),
+    article_id: articleLookup[belongs_to],
+  }));
+  return formattedCommentData;
+};
